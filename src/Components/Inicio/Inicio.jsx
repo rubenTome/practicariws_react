6afa79dacdccc52630b46f2787283e0ecb96db61
rubenTome <rubentome27@gmail.com>
2023-10-17 00:25:53 +0200

    primera version boton redondo TODO acciones asociadas, estilo

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import {Button} from "react-native"
 import Grid from '@mui/material/Grid';
 
+const botonRedondo = {
+    background:"white",
+    marginLeft: "12.25em",
+    marginTop: "1.5em",
+    width: "12.5em",
+    height:"12.5em",
+    fontSize: "1.25em",
+    borderRadius: "50%"
+}
 
 const Inicio = ({url, fondo}) => {
     return (
         <main style={{backgroundImage: `url(${fondo})`}}>
             <Grid container>
                 <Grid item xs={5} style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-                    <Button title="¡Comienza a buscar!" color="#a7b9d3"/>
+                    <button style={botonRedondo}>! COMIENZA A BUSCAR ¡</button>
                 </Grid>
                 <Grid item xs={7}>
                     {/*15.46em totales de padding vertical para que el fondo ocupe toda la pantalla*/}
-                    <img style={{paddingTop: "7.73em", paddingBottom:"7.73em", paddingRight: "44em"}} src={url} alt="OAK"/>
+                    <img style={{paddingTop: "12%", paddingBottom:"12%"}} src={url} alt="OAK"/>
                 </Grid>
             </Grid>
         </main>
