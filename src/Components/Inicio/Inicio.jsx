a39645cf7d4b83d2af42e83feb1a6553911b04bb
rubenTome <rubentome27@gmail.com>
2023-10-20 16:06:08 +0200

    estilo boton redondo terminado

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -4,22 +4,12 @@ import { useNavigate } from "react-router-dom";
 
 import oak from "./../../Assets/Images/oak.png"
 import fondo from "./../../Assets/Images/fondo_pokedex_2.jpg"
-
-const botonRedondo = {
-    background:"white",
-    marginLeft: "12.25em",
-    marginTop: "1.5em",
-    width: "12.5em",
-    height:"12.5em",
-    fontSize: "1.25em",
-    borderRadius: "50%"
-}
+import "./Inicio.css"
 
 const Inicio = () => {
     const navigate = useNavigate()
 
     const cambiarPag = () => {
-        console.log("CAMBIAR PAGINA !!!")
         navigate("/buscador")
     }
 
@@ -27,7 +17,7 @@ const Inicio = () => {
         <main style={{backgroundImage: `url(${fondo})`}}>
             <Grid container>
                 <Grid item xs={5} style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-                    <button style={botonRedondo} onClick={cambiarPag}>! COMIENZA A BUSCAR ¡</button>
+                    <button className="botonRedondo" onClick={cambiarPag}>! COMIENZA A BUSCAR ¡</button> 
                 </Grid>
                 <Grid item xs={7}>
                     {/*15.46em totales de padding vertical para que el fondo ocupe toda la pantalla*/}
