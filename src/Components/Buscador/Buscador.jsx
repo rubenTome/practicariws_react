54e444b1b457783ad17e7f396642f43bea031094
rubenTome <rubentome27@gmail.com>
2023-10-28 11:44:57 +0200

    switch selector posicionado TODO acabar buscador

--- src/Components/Buscador/Buscador.jsx
+++ src/Components/Buscador/Buscador.jsx
@@ -1,6 +1,46 @@
+import { React } from "react"
+import SwitchSelector from "react-switch-selector" //version 1.2.1
+import Grid from '@mui/material/Grid';
+
 const Buscador = () => {
+    const options = [
+        {
+            label: "Nombre",
+            value: "nombre",
+            selectedBackgroundColor: "red",
+            selectedFontColor: "black"
+        },
+        {
+            label: "Tipo",
+            value: "tipo",
+            selectedBackgroundColor: "white",
+            selectedFontColor: "black"
+        },
+        {
+            label: "Rango",
+            value: "rango",
+            selectedBackgroundColor: "greenyellow",
+            selectedFontColor: "black"
+        }
+    ];
+    
+    const onChange = (newValue) => {
+        console.log(newValue);
+    };
+    
+    const initialSelectedIndex = options.findIndex(({value}) => value === "VALOR");
+    
     return (
-        <div>BUSCADOR</div>
+        <main>
+            <div style={{padding:"2.5em", height:"3em"}}>
+                <SwitchSelector
+                        onChange={onChange}
+                        options={options}
+                        initialSelectedIndex={initialSelectedIndex}
+                        backgroundColor={"#353b48"}
+                        fontColor={"#f5f6fa"}/>
+            </div>
+        </main>
     )
 }
 
