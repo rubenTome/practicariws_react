34ba99ecfaf2299a8e1c8793178b6e80462212a1
rubenTome <rubentome27@gmail.com>
2023-10-28 20:04:42 +0200

    form con el eventListener Todo enviar y recibir info de elastic

--- src/Components/Buscador/Buscador.jsx
+++ src/Components/Buscador/Buscador.jsx
@@ -29,16 +29,61 @@ const Buscador = () => {
     };
     
     const initialSelectedIndex = options.findIndex(({value}) => value === "VALOR");
+
+    function enviarNombre(event) {
+        log.textContent = input.value;
+        event.preventDefault();
+    }
+      
+    const form = document.getElementById("formulario");
+    const log = document.getElementById("resultado");
+    const input = document.getElementById("nombre");
     
+    if(form) {
+        form.addEventListener("submit", enviarNombre);
+    }
+    //var request = new XMLHttpRequest()
+    //request.open("POST", "http://localhost:9200/pokemon/_search")
+    //request.send(JSON.stringify({"query":{"bool":{"must":[{"match":{"Nombre":input.value}}],"must_not":[],"should":[]}},"from":0,"size":1000,"sort":[],"aggs":{}}))
+    //request.addEventListener('load', () => {
+    //    console.log(request.responseText)
+    //})
+
     return (
-        <main>
-            <div style={{padding:"2.5em", height:"3em"}}>
+        <main 
+            style={{background:"blue"}}>
+            <div 
+                style={{padding:"2.5em", height:"3em", background:"green"}}>
                 <SwitchSelector
                         onChange={onChange}
                         options={options}
                         initialSelectedIndex={initialSelectedIndex}
                         backgroundColor={"#353b48"}
                         fontColor={"#f5f6fa"}/>
+            </div>
+            <div 
+                style={{background:"red"}}>
+                <div 
+                style={{padding:"23.3em", background:"yellow"}}>
+                    {/*NUEVO COMPONENTE*/}
+                    <Grid 
+                        container
+                        direction="column"
+                        justifyContent="center"
+                        alignItems="center">
+                        <Grid item xs={8}>
+                            <form id="formulario">
+                                <label>
+                                    Nombre del pokémon:
+                                </label><br/>
+                                <input type="text" id="nombre" name="nombre"/>
+                                <input type="submit" value="buscar"/>
+                            </form>
+                        </Grid>
+                        <Grid item xs={8}><p id="resultado"/></Grid>
+                    </Grid>
+                    {/*NUEVO COMPONENTE*/}
+                </div>
             </div>
         </main>
     )
--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -17,7 +17,7 @@ const Inicio = () => {
         <main style={{backgroundImage: `url(${fondo})`}}>
             <Grid container>
                 <Grid item xs={5} style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-                    <button className="botonRedondo" onClick={cambiarPag}>! COMIENZA A BUSCAR ¡</button> 
+                    <button className="botonRedondo" onClick={cambiarPag}>¡ COMIENZA A BUSCAR !</button> 
                 </Grid>
                 <Grid item xs={7}>
                     {/*15.46em totales de padding vertical para que el fondo ocupe toda la pantalla*/}
